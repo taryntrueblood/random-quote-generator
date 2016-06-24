1883427e6cc4cb7a3639fcf2f3ea79128b1d74a2
Taryn Trueblood <taryntrueblood@yahoo.com>
2016-06-24 16:42:23 -0500

    update file

--- js/script.js
+++ js/script.js
@@ -15,26 +15,30 @@ var quotes = [
   {
     quote: "When you do the common things in life in an uncommon way, you will command the attention of the world.",
     source: "George Washington Carver"
+  },
+  {
+    quote:"A lie gets halfway around the world before the truth has a chance to get its pants on.",
+    source: "Sir Winston Churchill"
   }
 ];
 
-
+var quote;
+var output = "";
 //create a function named getRandomQuote
 function getRandomQuote() {
   //select a random quote from the quotes array
   var random = Math.floor(Math.random() * quotes.length);
-  for(var prop in quotes[random]){
-     document.getElementById('quote-box').innerHTML = quotes[random][prop];
-   }
+  for(var i = 0; i < quotes.length; i++){
+    quote = quotes[random];
+    output = "<p class='quote'>"+ quote.quote +"</p>";
+    output += "<p class='source'>"+ quote.source + "</p>";
+  }
+    document.getElementById('quote-box').innerHTML = output;
  }
 
 //create a function called printQuote
 function printQuote(){
     var randomQuote = getRandomQuote();
-
-    var quote = "<p class='quote'>"+ "</p>";
-        quote += "<p class='source'>" + "</p>";
-     document.getElementById('quote-box').innerHTML = quote;
      return randomQuote;
 }
 
